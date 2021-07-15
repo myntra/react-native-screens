d7cddc8caaab701398a17ad09045f4b1151e9a07
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2021-07-15 11:07:28 +0200

    feat: clean event dispatching on Android (#1014)
    Clean up event dispatching code on Android.

--- android/src/main/java/com/swmansion/rnscreens/ScreenFragment.java
+++ android/src/main/java/com/swmansion/rnscreens/ScreenFragment.java
@@ -11,6 +11,7 @@ import android.widget.FrameLayout;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.UiThreadUtil;
 import com.facebook.react.uimanager.UIManagerModule;
+import com.facebook.react.uimanager.events.Event;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -119,46 +120,51 @@ public class ScreenFragment extends Fragment {
   }
 
   protected void dispatchOnWillAppear() {
-    ((ReactContext) mScreenView.getContext())
-        .getNativeModule(UIManagerModule.class)
-        .getEventDispatcher()
-        .dispatchEvent(new ScreenWillAppearEvent(mScreenView.getId()));
-
-    dispatchEventInChildContainers(ScreenLifecycleEvent.WillAppear);
+    dispatchEvent(ScreenLifecycleEvent.WillAppear, this);
   }
 
   protected void dispatchOnAppear() {
-    ((ReactContext) mScreenView.getContext())
-            .getNativeModule(UIManagerModule.class)
-            .getEventDispatcher()
-            .dispatchEvent(new ScreenAppearEvent(mScreenView.getId()));
-
-    dispatchEventInChildContainers(ScreenLifecycleEvent.Appear);
+    dispatchEvent(ScreenLifecycleEvent.Appear, this);
   }
 
   protected void dispatchOnWillDisappear() {
-    ((ReactContext) mScreenView.getContext())
-        .getNativeModule(UIManagerModule.class)
-        .getEventDispatcher()
-        .dispatchEvent(new ScreenWillDisappearEvent(mScreenView.getId()));
-
-    dispatchEventInChildContainers(ScreenLifecycleEvent.WillDisappear);
+    dispatchEvent(ScreenLifecycleEvent.WillDisappear, this);
   }
 
   protected void dispatchOnDisappear() {
-    ((ReactContext) mScreenView.getContext())
+    dispatchEvent(ScreenLifecycleEvent.Disappear, this);
+  }
+
+  private void dispatchEvent(ScreenLifecycleEvent event, ScreenFragment fragment) {
+    Screen screen = fragment.getScreen();
+    Event lifecycleEvent;
+    switch (event) {
+      case WillAppear:
+        lifecycleEvent = new ScreenWillAppearEvent(screen.getId());
+        break;
+      case Appear:
+        lifecycleEvent = new ScreenAppearEvent(screen.getId());
+        break;
+      case WillDisappear:
+        lifecycleEvent = new ScreenWillDisappearEvent(screen.getId());
+        break;
+      case Disappear:
+        lifecycleEvent = new ScreenDisappearEvent(screen.getId());
+        break;
+      default:
+        throw new IllegalStateException("Tried to dispatch unknown event: " + event.toString());
+    }
+    ((ReactContext) screen.getContext())
         .getNativeModule(UIManagerModule.class)
         .getEventDispatcher()
-        .dispatchEvent(new ScreenDisappearEvent(mScreenView.getId()));
+        .dispatchEvent(lifecycleEvent);
 
-    dispatchEventInChildContainers(ScreenLifecycleEvent.Disappear);
+    fragment.dispatchEventInChildContainers(event);
   }
 
   private void dispatchEventInChildContainers(ScreenLifecycleEvent event) {
     for (ScreenContainer sc : mChildScreenContainers) {
       if (sc.getScreenCount() > 0) {
-        Screen topScreen = sc.getScreenAt(sc.getScreenCount() - 1);
-        topScreen.getFragment().dispatchOnDisappear();
         Screen topScreen = sc.getTopScreen();
         if (topScreen != null
             && topScreen.getFragment() != null
@@ -169,25 +175,6 @@ public class ScreenFragment extends Fragment {
           dispatchEvent(event, topScreen.getFragment());
         }
       }
-    }
-  }
-
-  private void dispatchEvent(ScreenLifecycleEvent event, ScreenFragment fragment) {
-    switch (event) {
-      case Appear:
-        fragment.dispatchOnAppear();
-        break;
-      case WillAppear:
-        fragment.dispatchOnWillAppear();
-        break;
-      case Disappear:
-        fragment.dispatchOnDisappear();
-        break;
-      case WillDisappear:
-        fragment.dispatchOnWillDisappear();
-        break;
-      default:
-        break;
     }
   }
 
